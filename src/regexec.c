1a519057d83f59393a4a07cc59747a02b220c4b2
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2020-06-04 15:47:48 +0200

    Fix undefined behavior: remove dangerous cast that may truncate the value of pointer differences.

--- src/regexec.c
+++ src/regexec.c
@@ -4850,7 +4850,8 @@ sunday_quick_search_step_forward(regex_t* reg,
 {
   const UChar *s, *se, *t, *p, *end;
   const UChar *tail;
-  int skip, tlen1;
+  int skip;
+  ptrdiff_t tlen1;
   int map_offset;
   OnigEncoding enc;
 
@@ -4863,7 +4864,7 @@ sunday_quick_search_step_forward(regex_t* reg,
   enc = reg->enc;
 
   tail = target_end - 1;
-  tlen1 = (int )(tail - target);
+  tlen1 = tail - target;
   end = text_range;
   if (tlen1 > text_end - end)
     end = text_end - tlen1;
